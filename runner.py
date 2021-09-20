e6dd79f3e5ff3e2d2bd52c1140a9dfd1ff290abb
VladislavShatov <86914645+VladislavShatov@users.noreply.github.com>
2021-09-20 21:03:58 +0300

    Add files via upload

---
+++ runner.py
@@ -0,0 +1,12 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+from bookparser import settings
+from bookparser.spiders.labirint import Labirint
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+    process = CrawlerProcess(settings=crawler_settings)
+    process.crawl(Labirint)
+    process.start()
